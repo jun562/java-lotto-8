1be667453a7c116956847a0215659a9d01c03289
jun562 <capboy290@naver.com>
2025-11-03 15:22:09 +0900

    feat(outputView): 로또 구입 개수 출력 로직 추가

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,13 @@
+package view;
+
+public class OutputView {
+
+    private OutputView() {
+
+    }
+
+    public static void printLottoCount(int lottoCount) {
+        System.out.println();
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+}
