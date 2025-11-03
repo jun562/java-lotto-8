c0366d623c00865c67efa79b9e6e12d38b3b44b4
jun562 <capboy290@naver.com>
2025-11-03 17:45:36 +0900

    feat(lottoService): 구입 개수에 따른 로또 발행 로직 추가

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,14 +1,29 @@
 package service;
 
 import constant.LottoRule;
+import domain.Lotto;
+import domain.LottoGenerator;
+import domain.LottoWallet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoService {
 
-    public LottoService() {
+    private final LottoGenerator lottoGenerator;
 
+    public LottoService(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
     }
 
     public int calculateLottoCount(int purchaseAmount) {
         return (purchaseAmount / LottoRule.LOTTO_PRICE_UNIT);
+    }
+
+    public LottoWallet generateLottoWallet(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < lottoCount; count++) {
+            lottos.add(lottoGenerator.generate());
+        }
+        return new LottoWallet(lottos);
     }
 }
