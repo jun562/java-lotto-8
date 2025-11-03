2a04da59ba347af68d6c98a9b278c6cedff6c99d
jun562 <capboy290@naver.com>
2025-11-03 22:20:46 +0900

    feat(main): 전체 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import controller.LottoController;
+import domain.LottoGenerator;
+import service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(lottoGenerator);
+        LottoController lottoController = new LottoController(lottoService);
+        lottoController.run();
+
     }
 }
