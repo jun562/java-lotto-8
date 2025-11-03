243e064073c053466a6233e40c81351f6fcd4ae7
jun562 <capboy290@naver.com>
2025-11-03 12:22:12 +0900

    refactor: 에러 메시지 추가 및 각 검증 단계 메서드 분리

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -3,7 +3,10 @@ package constant;
 public enum ErrorMessage {
 
     NON_NUMERIC("입력은 숫자여야 합니다."),
-    NOT_NUMBER_IN_LIST("당첨 번호 목록에 숫자가 아닌 값이 포함되어 있습니다.");
+    NOT_NUMBER_IN_LIST("당첨 번호 목록에 숫자가 아닌 값이 포함되어 있습니다."),
+    NOT_POSITIVE_PURCHASE_AMOUNT("구입 금액은 0보다 커야 합니다."),
+    INVALID_PURCHASE_AMOUNT_UNIT("구입 금액은 1,000원 단위여야 합니다."),
+    ;
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/view/Validator.java
+++ src/main/java/view/Validator.java
@@ -9,12 +9,19 @@ public class Validator {
     }
 
     public static void validatePurchaseAmount(int number) {
+        validateNegative(number);
+        validateAmountUnit(number);
+    }
+
+    private static void validateNegative(int number) {
         if (number <= 0) {
             throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE_PURCHASE_AMOUNT.getMessage());
         }
+    }
+
+    private static void validateAmountUnit(int number) {
         if (number % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
         }
     }
-
 }
