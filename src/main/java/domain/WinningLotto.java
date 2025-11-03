c26451c584cf862d3b725f7de57b52eb3a81e8f4
jun562 <capboy290@naver.com>
2025-11-03 19:19:13 +0900

    feat(winningLotto): 당첨 정보 포함하는 로또 생성 로직 추가

---
+++ src/main/java/domain/WinningLotto.java
@@ -0,0 +1,24 @@
+package domain;
+
+import constant.ErrorMessage;
+import constant.LottoRule;
+
+public class WinningLotto {
+    private final Lotto lotto; // 내부적으로 검증된 객체 이기에 별도의 검증 x
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber) {
+        validate(lotto, bonusNumber);
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto lotto, int bonusNumber) {
+        if (bonusNumber < LottoRule.MIN_LOTTO_NUMBER || bonusNumber > LottoRule.MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_RANGE.getMessage());
+        }
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
+    }
+}
