a9b7ca47e44a82e82025a0ab30dad7a64c367c31
jun562 <capboy290@naver.com>
2025-11-03 20:27:48 +0900

    test(unit): 로또 개수에 따른 당첨 등수 처리 단위 테스트 추가

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import constant.LottoRank;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,5 +40,62 @@ class LottoTest {
     void validateWhenOutOfRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 51)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static final Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    @Test
+    @DisplayName("6개_번호_일치_시_1등을_반환한다")
+    void matchFirstRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 7);
+
+        assertEquals(LottoRank.FIRST, lotto.match(winningLotto));
+    }
+
+    // --- 2. 2등 테스트 (5개 일치 + 보너스) ---
+    @Test
+    @DisplayName("5개_번호와_보너스_일치_시_2등을_반환한다")
+    void matchSecondRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 3, 4, 5, 10));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 6);
+
+        assertEquals(LottoRank.SECOND, lotto.match(winningLotto));
+    }
+
+    @Test
+    @DisplayName("5개_번호_일치_시_3등을_반환한다")
+    void matchThirdRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 3, 4, 5, 10));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 7);
+
+        assertEquals(LottoRank.THIRD, lotto.match(winningLotto));
+    }
+
+    @Test
+    @DisplayName("4개_번호_일치_시_4등을_반환한다")
+    void matchFourthRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 3, 4, 11, 10));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 7);
+
+        assertEquals(LottoRank.FOURTH, lotto.match(winningLotto));
+    }
+
+    @Test
+    @DisplayName("3개_번호_일치_시_5등을_반환한다")
+    void matchFifthRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 3, 12, 11, 10));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 7);
+
+        assertEquals(LottoRank.FIFTH, lotto.match(winningLotto));
+    }
+
+    @Test
+    @DisplayName("2개_이하_일치_시_꽝을_반환한다")
+    void matchMissRank() {
+        Lotto winningNums = new Lotto(List.of(1, 2, 40, 41, 42, 43));
+        WinningLotto winningLotto = new WinningLotto(winningNums, 7);
+
+        assertEquals(LottoRank.MISS, lotto.match(winningLotto));
     }
 }
