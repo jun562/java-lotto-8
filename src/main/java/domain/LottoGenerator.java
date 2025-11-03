ff6bdd7c75a508d32dcf22f65ae9f1a01b89e834
jun562 <capboy290@naver.com>
2025-11-03 17:00:04 +0900

    feat(lottoGenerator): 로또 발행 로직 추가

---
+++ src/main/java/domain/LottoGenerator.java
@@ -0,0 +1,21 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import constant.LottoRule;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public LottoGenerator() {
+
+    }
+
+    public Lotto Generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LottoRule.MIN_LOTTO_NUMBER,
+                LottoRule.MAX_LOTTO_NUMBER,
+                LottoRule.LOTTO_NUMBER_COUNT
+        );
+        return new Lotto(numbers);
+    }
+}
