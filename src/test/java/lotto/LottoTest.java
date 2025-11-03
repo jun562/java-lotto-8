eafe27140d19af8112ac6d1841c60891f9713a2e
jun562 <capboy290@naver.com>
2025-11-03 16:34:39 +0900

    test(unit): 로또 생성 검증 로직 단위 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,25 +1,32 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.Lotto;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+    @DisplayName("로또_번호_개수가_6이_아닌_경우")
+    void validateWhenCountNotSix() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+    @DisplayName("로또_번호가_중복되는_경우")
+    void validateWhenDuplicated() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또_번호가_범위를_벗어난_경우")
+    void validateWhenOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 51)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
