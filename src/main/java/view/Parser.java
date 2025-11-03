44799692e6398e6db7e27874c1370ac4b381cae2
jun562 <capboy290@naver.com>
2025-11-03 10:29:19 +0900

    feat(parser): 정수 파싱 로직 구현

--- src/main/java/view/Parser.java
+++ src/main/java/view/Parser.java
@@ -2,6 +2,15 @@ package view;
 
 public class Parser {
     private Parser() {
-        
+
+    }
+
+    public static int parseStringToInteger(String input) {
+        String trimmedInput = input.trim();
+        try {
+            return Integer.parseInt(trimmedInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자를 입력해야 합니다.");
+        }
     }
 }
