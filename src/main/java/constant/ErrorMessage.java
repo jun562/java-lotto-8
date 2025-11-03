e48a53a4b70bfaa308c978050b21e6f03f474521
jun562 <capboy290@naver.com>
2025-11-03 11:34:51 +0900

    feat(parser): 정수형 리스트 파싱 로직 추가

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,7 +1,8 @@
 package constant;
 
 public enum ErrorMessage {
-    NON_NUMERIC("입력은 숫자여야 합니다.");
+    NON_NUMERIC("입력은 숫자여야 합니다."),
+    NOT_NUMBER_IN_LIST("당첨 번호 목록에 숫자가 아닌 값이 포함되어 있습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/view/Parser.java
+++ src/main/java/view/Parser.java
@@ -1,8 +1,12 @@
 package view;
 
 import constant.ErrorMessage;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser {
+    private static final String DELIMITER = ",";
+
     private Parser() {
 
     }
@@ -12,6 +16,16 @@ public class Parser {
             return Integer.parseInt(trimInput(input));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC.getMessage());
+        }
+    }
+
+    public static List<Integer> parseStringToList(String input) {
+        List<String> splitInput = List.of(input.split(DELIMITER));
+        List<String> trimmedInput = splitInput.stream().map(String::trim).toList();
+        try {
+            return trimmedInput.stream().map(Integer::parseInt).collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_IN_LIST.getMessage());
         }
     }
 
