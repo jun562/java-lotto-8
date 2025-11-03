68050bca01d286c18f2672a014d281385803a058
jun562 <capboy290@naver.com>
2025-11-03 15:34:53 +0900

    feat(outputView): 에러 메시지 출력 로직 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,4 +10,8 @@ public class OutputView {
         System.out.println();
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
 }
