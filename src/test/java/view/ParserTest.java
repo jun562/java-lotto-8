cbe8d88544032120f115ca156b836cff0d4d8fdf
jun562 <capboy290@naver.com>
2025-11-03 10:22:37 +0900

    test(unit): 숫자가 아닌 경우의 예외 처리 로직 단위 테스트 추가

--- src/test/java/view/ParserTest.java
+++ src/test/java/view/ParserTest.java
@@ -1,9 +1,12 @@
 package view;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ParserTest {
 
@@ -25,5 +28,19 @@ class ParserTest {
         int result = Parser.parseStringToInteger(purchaseAmount);
 
         assertEquals(1000, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "abc", // 숫자가 아닌 문자열인 경우
+            "1000.123", // 실수인 경우
+    })
+    @DisplayName("로또_구입_금액이_숫자가_아닌_경우")
+    void parseStringToIntegerWhenNotInteger(String purchaseAmount) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Parser.parseStringToInteger(purchaseAmount);
+        });
+
+        assertEquals("[ERROR] 로또 구입 금액은 숫자를 입력해야 합니다.", exception.getMessage());
     }
 }
