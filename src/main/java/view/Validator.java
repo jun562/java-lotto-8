78c0ca343ffa48b6d0299a74fbfd47929d853984
jun562 <capboy290@naver.com>
2025-11-03 13:13:38 +0900

    refactor: 당첨 번호 검증 로직 메서드 분리

--- src/main/java/view/Validator.java
+++ src/main/java/view/Validator.java
@@ -10,7 +10,8 @@ public class Validator {
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    
+    private static final int LOTTO_PRICE_UNIT = 1000;
+
     private Validator() {
 
     }
@@ -21,18 +22,9 @@ public class Validator {
     }
 
     public static void validateWinningLotto(List<Integer> winningLotto) {
-        if (winningLotto.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
-        }
-        Set<Integer> uniqueNumbers = new HashSet<>(winningLotto);
-        if (uniqueNumbers.size() != winningLotto.size()) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBERS_IN_LOTTO.getMessage());
-        }
-        for (int number : winningLotto) {
-            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-                throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_RANGE.getMessage());
-            }
-        }
+        validateLottoSize(winningLotto);
+        validateLottoDuplicated(winningLotto);
+        validateLottoRange(winningLotto);
     }
 
     private static void validateNegative(int number) {
@@ -42,8 +34,29 @@ public class Validator {
     }
 
     private static void validateAmountUnit(int number) {
-        if (number % 1000 != 0) {
+        if (number % LOTTO_PRICE_UNIT != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
+        }
+    }
+
+    private static void validateLottoSize(List<Integer> winningLotto) {
+        if (winningLotto.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
+        }
+    }
+
+    private static void validateLottoDuplicated(List<Integer> winningLotto) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningLotto);
+        if (uniqueNumbers.size() != winningLotto.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBERS_IN_LOTTO.getMessage());
+        }
+    }
+
+    private static void validateLottoRange(List<Integer> winningLotto) {
+        boolean isOutOfRange = winningLotto.stream()
+                .anyMatch(number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER);
+        if (isOutOfRange) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_RANGE.getMessage());
         }
     }
 }
